1bad069de8cd13aa94ea0b92c5a555bb1eed9827
lukacvetkovic <l.cvetk@gmail.com>
2015-03-29 10:34:42 +0200

    obrisano nepotrebno

--- app/src/main/java/parkme/projectm/hr/parkme/Database/OrmliteDb/DatabaseConfigUtil.java
+++ app/src/main/java/parkme/projectm/hr/parkme/Database/OrmliteDb/DatabaseConfigUtil.java
@@ -1,22 +1,17 @@
 package parkme.projectm.hr.parkme.Database.OrmliteDb;
 
 
-import android.util.Log;
-
 import com.j256.ormlite.android.apptools.OrmLiteConfigUtil;
 
 import parkme.projectm.hr.parkme.Database.OrmliteDb.Models.City;
-import parkme.projectm.hr.parkme.Database.OrmliteDb.Models.Marker;
 import parkme.projectm.hr.parkme.Database.OrmliteDb.Models.ParkingZone;
 import parkme.projectm.hr.parkme.Database.OrmliteDb.Models.PaymentMode;
-import parkme.projectm.hr.parkme.R;
 /**
  * Created by ppx10 on 29.03.15..
  */
 public class DatabaseConfigUtil extends OrmLiteConfigUtil {
     private static final Class<?>[] classes = new Class[] {
             City.class,
-            Marker.class,
             ParkingZone.class,
             PaymentMode.class,
     };
