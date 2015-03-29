a67aa16402cefb7c9b695f4d2e4c159154be5556
lukacvetkovic <l.cvetk@gmail.com>
2015-03-29 15:39:56 +0200

    Dialog popravljen

--- app/src/main/java/parkme/projectm/hr/parkme/Dialogs/ConfirmPaymentDialog.java
+++ app/src/main/java/parkme/projectm/hr/parkme/Dialogs/ConfirmPaymentDialog.java
@@ -6,6 +6,7 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.widget.TextView;
 
 import org.apache.http.client.params.ClientPNames;
 
@@ -15,6 +16,34 @@ import parkme.projectm.hr.parkme.R;
  * Created by Cveki on 29.3.2015..
  */
 public class ConfirmPaymentDialog extends DialogFragment {
+
+    TextView tvTime;
+    TextView tvCity;
+    TextView tvParkingZone;
+    TextView tvPrice;
+    TextView tvDuration;
+    TextView tvMaxDuration;
+
+    String city;
+    String parkingZone;
+    float price;
+    String duration;
+    String maxDuarton;
+    String carPlate;
+    String parkingZoneNumber;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        tvTime=(TextView)getView().findViewById(R.id.tvTime);
+        tvCity=(TextView)getView().findViewById(R.id.tvCity);
+        tvParkingZone=(TextView)getView().findViewById(R.id.tvParkingZone);
+        tvPrice=(TextView)getView().findViewById(R.id.tvPrice);
+        tvDuration=(TextView)getView().findViewById(R.id.tvDuration);
+        tvMaxDuration=(TextView)getView().findViewById(R.id.tvMaxDuration);
+
+
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -37,6 +66,7 @@ public class ConfirmPaymentDialog extends DialogFragment {
                         ConfirmPaymentDialog.this.getDialog().cancel();
                     }
                 });
+
         return builder.create();
     }
 }
